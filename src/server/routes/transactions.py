6de4c466554fba82a71eba4b6c96a13b0969d11a
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2021-02-16 01:39:50 -0800

    Correccion de estilos

--- src/server/routes/transactions.py
+++ src/server/routes/transactions.py
@@ -2,6 +2,7 @@ import bottle
 
 app = bottle.Bottle()
 
+
 @app.get("<transaction_id>")
 def transactions_get_by_id(transactions_id):
-    return {"code":200,"message":"Bajo construccion"}
+    return {"code": 200, "message": "Bajo construccion"}
