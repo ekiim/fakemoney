dc08dccae94e193f169ab97a12e2bf8c423b6634
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2021-02-16 02:22:08 -0800

    Corrected test script to ensure exit code.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 echo "testing"
 cd src/ && python -m unittest -v tests/*
+[[ "$?" != "0" ]] && exit 1
+exit 0
--- src/tests/test_00_tautology.py
+++ src/tests/test_00_tautology.py
@@ -4,3 +4,6 @@ import unittest
 class TautologyTest(unittest.TestCase):
     def test_tautology(self):
         assert True
+
+    # def test_fallacy(self):
+    #    assert False
