21f4a7fda8952653db4e50992f294d720e5bae6a
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2021-02-26 18:10:15 -0800

    Corrected test script to display result at the end.

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,5 @@ cd src && coverage run --source="./" --omit="./tests" -m unittest -v tests/*
 [[ "$?" != "0" ]] && ERROR=1
 coverage report
 coverage html -d  ../www/docs/python
+[[ "$ERROR" != "0" ]] && echo "Tests Failed" || echo "Tests Passed"
 exit $ERROR
