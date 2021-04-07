b73b5f64264753a8e7a405aecf6eeb312de51803
Miguel Alejandro Salgado Zapien <mikesalzap@gmail.com>
2021-04-06 22:29:32 -0700

    Script para iniciar el servidor completo.

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,5 +4,7 @@ trap 'kill $(jobs -p)' EXIT
 PORT=${1:-8091}
 echo "Serving http server. $PORT"
 python -m http.server --directory ./www $PORT &
+WWW_PID=$$
 cd src/ && python -m server
+kill -s 9 ${WWW_PID}
 echo "Safe Quit"
